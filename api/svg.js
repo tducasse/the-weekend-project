5693224f0449940b90931bdd0abbb6705dc1e54f
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2020-09-19 13:33:47 +1000

    Add CORS and content-type

--- api/svg.js
+++ api/svg.js
@@ -1,7 +1,26 @@
 const https = require('https');
 const URL = `https://img.shields.io/badge/weekend--project-what's%20this%3F-blue?link=&link=https://github.com/tducasse/the-weekend-project`;
 
-module.exports = (req, res) => {
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  res.setHeader('Content-Type', 'image/svg+xml');
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
   https.get(URL, (resp) => {
     let data = '';
 
@@ -17,3 +36,6 @@ module.exports = (req, res) => {
     console.log("Error: " + err.message);
   });
 }
+
+module.exports = allowCors(handler)
+
