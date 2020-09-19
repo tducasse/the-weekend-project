8e9a6bc5cefff84628643c2443d882cdf10eb535
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2020-09-20 01:09:51 +1000

    Return markdown

--- api/list.js
+++ api/list.js
@@ -1,6 +1,6 @@
 const { Octokit } = require("@octokit/rest");
 const octokit = new Octokit(
-  { auth: process.env.OAUTH_GITHUB }
+  { auth: process.env.OAUTH_GITHUB, userAgent: "the-weekend-project" }
 );
 
 const allowCors = fn => async (req, res) => {
@@ -20,6 +20,23 @@ const allowCors = fn => async (req, res) => {
   return await fn(req, res)
 }
 
+const getWeekendProjects = (readmes) => {
+  const repos = [];
+  readmes.forEach(
+    readme => {
+      const data = readme.result && readme.result.data;
+      if (!data) {
+        return false;
+      }
+      if (data.indexOf('![weekend-project](https://the-weekend-project.vercel.app/api/svg)]') < 0) {
+        return false;
+      }
+      repos.push(`- [${readme.repo.name}](${readme.repo.url})`);
+    }
+  );
+  return repos.join('\n');
+}
+
 const handler = async (req, res) => {
   const { user } = req.query
   if (!user) {
@@ -35,25 +52,22 @@ const handler = async (req, res) => {
     per_page: 100,
   });
 
-  const repos = (response.data || []).map(repo => repo.name)
+  const repos = (response.data || []).map(repo => ({ name: repo.name, url: repo.html_url }))
 
   const readmes = await Promise.all(
     repos.map(
-      async repo => {
-        try {
-          return octokit.repos.getReadme({
-            owner: user,
-            repo,
-          });
-        } catch (e) {
-          return false;
-        }
-      }
-    ).filter(Boolean)
+      repo =>
+        octokit.repos.getReadme({
+          owner: user,
+          repo: repo.name,
+          headers: {
+            "Accept": "application/vnd.github.v3.raw"
+          }
+        }).then((result) => ({ result, repo })).catch(() => false)
+    )
   )
 
-
-  res.send(readmes[0])
+  res.send(getWeekendProjects(readmes));
 }
 
 module.exports = allowCors(handler)
